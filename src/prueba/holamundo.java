a46a72ad8bb1b9a8d944f8deb145342305b5bc90
jose rojas <jcontrareas@gmail.com>
2020-03-24 23:52:08 -0400

    [ADD] agregando prueba al repositorio

---
+++ src/prueba/holamundo.java
@@ -0,0 +1,12 @@
+package prueba;
+
+import javax.swing.JOptionPane;
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Bienvenido Todos Ustedes Yo soy Dreyka Santana");
+
+	}
+
+}
